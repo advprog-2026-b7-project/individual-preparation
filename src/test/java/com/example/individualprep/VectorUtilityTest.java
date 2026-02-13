516242048f9e5f50ab2288a9c571f359cf08cb1f
amberleyvs <amberley.vidya41@ui.ac.id>
2026-02-13 14:29:50 +0700

    feat: implement vector subtract testing

--- src/test/java/com/example/individualprep/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/VectorUtilityTest.java
@@ -11,4 +11,14 @@ class VectorUtilityTest {
         double[] v1 = {3.0, 4.0};
         assertEquals(5.0, util.norm(v1), 0.0001);
     }
+
+    @Test
+    void testSubtract() {
+        VectorUtility util = new VectorUtility();
+        double[] v1 = {5.0, 7.0};
+        double[] v2 = {2.0, 3.0};
+        double[] result = util.subtract(v1, v2);
+        assertArrayEquals(new double[]{3.0, 4.0}, result, 0.0001);
+    }
 }
+
