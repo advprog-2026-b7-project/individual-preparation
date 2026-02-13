bf6b098e7bb87247550a4eb40ca6a5f1b0506134
satriobismaa <bismazharfan@gmail.com>
2026-02-13 14:44:54 +0700

    add unit test for multiply vector

--- src/test/java/com/example/individualprep/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/VectorUtilityTest.java
@@ -11,4 +11,12 @@ class VectorUtilityTest {
         double[] v1 = {3.0, 4.0};
         assertEquals(5.0, util.norm(v1), 0.0001);
     }
+
+    @Test
+    void testMultiply() {
+        VectorUtility util = new VectorUtility();
+        double[] input = {6.0, 7.0};
+        double[] result = {12.0, 14.0};
+        assertArrayEquals(result, util.multiply(input, 2));
+    }
 }
